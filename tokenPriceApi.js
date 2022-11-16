35244f7258ba794be0819f5379d32ab96c48b8fa
Payam Aryanfard <payam.aryanfard@gmail.com>
2022-11-16 06:18:39 -0800

    Manifest & Some meta added

--- tokenPriceApi.js
+++ tokenPriceApi.js
@@ -2,9 +2,9 @@ let pancakeSwapAbi = [{ "inputs": [{ "internalType": "uint256", "name": "amountI
 let tokenAbi = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },];
 
 let pancakeSwapContract = "0x10ED43C718714eb63d5aA57B78B54704E256024E".toLowerCase();
-const oweb3 = new Web3("https://bsc-dataseed1.binance.org");
+const oweb3 = new Web3("https://bsc-dataseed.binance.org/");
 async function calcSell(tokensToSell, tokenAddress) {
-    const oweb3 = new Web3("https://bsc-dataseed1.binance.org");
+    const oweb3 = new Web3("https://bsc-dataseed.binance.org/");
     const BNBTokenAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" //BNB
 
     let tokenRouter = await new oweb3.eth.Contract(tokenAbi, tokenAddress);
@@ -22,7 +22,7 @@ async function calcSell(tokensToSell, tokenAddress) {
     return amountOut;
 }
 async function calcBNBPrice() {
-    const oweb3 = new Web3("https://bsc-dataseed1.binance.org");
+    const oweb3 = new Web3("https://bsc-dataseed.binance.org/");
     const BNBTokenAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" //BNB
     const USDTokenAddress = "0x55d398326f99059fF775485246999027B3197955" //USDT
     let bnbToSell = oweb3.utils.toWei("1", "ether");
